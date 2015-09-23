29bfe0dc4924d131bcbc50bc5f479c83cfb6a323
Mritunjay Kumar <ashupost@gmail.com>
2015-09-23 06:57:40 +0530

    commit

--- src/main/java/com/ground/inventory/database/ComponentConsumer.java
+++ src/main/java/com/ground/inventory/database/ComponentConsumer.java
@@ -8,6 +8,7 @@ public class ComponentConsumer {
 
 	@Autowired
 	private SimpleWriter component;
+	
 	@Autowired
 	private SimpleWriterService service;
 
--- src/main/java/com/ground/inventory/database/Config.java
+++ src/main/java/com/ground/inventory/database/Config.java
@@ -1,16 +1,16 @@
 package com.ground.inventory.database;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan("com.ground.inventory.database")
+@EnableAspectJAutoProxy
+@ImportResource("classpath:/spring-xml/spring-resource.xml")
 public class Config {
 	
-	@Bean(name="helloBean")
-    public HelloWorld helloWorld() {
-        return new HelloWorldImpl();
-    }
+
 	
 }
